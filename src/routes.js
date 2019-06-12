45182fc1a4d73e50db3ffe111b0b06853a384e90
leoamiranda2 <leoamiranda2@gmail.com>
2019-06-11 21:13:04 -0300

    comentário no arquivo de rotas

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,17 @@
+// busca bibliotecas
 const express = require('express');
 const multer = require('multer');
+// busca configuração para upload de arquivo
 const uploadConfig = require('./config/upload');
+// importa models
 const PostController = require('./controllers/PostController');
 const LikeController = require('./controllers/LikeController');
 
+// inicializa objetos de rota e gerenciamento de multidata
 const routes = new express.Router();
 const upload = multer(uploadConfig);
+
+
 
 // metodo cadastrar post chamando a função do controller desejado
 routes.post('/posts', upload.single('image'), PostController.store);
@@ -14,6 +20,8 @@ routes.get('/posts', PostController.index);
 
 // :id se refere ao id do post no banco que esta sendo enviado como parametro
 routes.post('/posts/:id/like', LikeController.store);
+
+
 
 // export routes
 module.exports = routes;
